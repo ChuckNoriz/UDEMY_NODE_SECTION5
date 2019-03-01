e12b7583d8168487b5505d5f388b94b3a027cabf
unknown <withoversoul@outlook.com>
2019-03-01 02:28:16 -0800

    Second Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+
+var app = express();
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear()); //function template not to repeat in renderings
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.engine('hbs', require('hbs').__express);
+
+
+
+//middleware with NEXT function without which APP doesnt move on eny request, so it must be called. Try without NEXT for proof
+app.use((req,res,next) =>{
+    let now = new Date().toString();
+    let log = `${now}: ${req.method}: ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) =>{
+        if(err){console.log('Unable to append')}
+    })
+
+    next();
+});
+
+//middleware without NEXT, so it will stop everything
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');
+    
+// });
+
+//Middleware for static pages. Ex: localhost:3000/help.html
+app.use(express.static(__dirname + '/public'));
+
+//Simple initial response
+app.get('/', (req,res) => res.render('home.hbs',{
+    pageTitle:'Home Page',
+    head:'This is Home Page'
+}));
+
+app.get('/help', (req,res) => res.render('help'))
+
+// rendering hbs mustache templage page
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        head:'This is About Page'
+    });
+});
+app.listen(3000);
