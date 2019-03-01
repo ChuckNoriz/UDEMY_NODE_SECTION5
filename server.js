10c1840bfc2d682719cd80befbeb277a6740a3b5
unknown <withoversoul@outlook.com>
2019-03-01 03:11:19 -0800

    setup start script & heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs')
+const fs = require('fs');
+const port = process.env.port || 3000;
 
 var app = express();
 
@@ -48,4 +49,4 @@ app.get('/about', (req, res) => {
         head:'This is About Page'
     });
 });
-app.listen(3000);
+app.listen(port, () => console.log('Running on port - ', port));
